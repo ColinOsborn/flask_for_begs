210b88337ebf0a3ef358c29a3fc12c8b9d0d09a6
Colin Osborn <colin.osborn@gmail.com>
2019-09-18 22:47:26 -0600

    implemented templates into app

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    return 'Hello Fuckers'
+    data = {{'name' : 'Python'}, {'name': 'Colin'}, {'name': 'Kimi'}}
+    return render_template('index.html', name="Colin", number=7 data=data)
 
 @app.route('/home')
-def home():
+def home_get():
     return 'Home'
+
+@app.route('/home', methods=['POST'])
+def home_post():
+    return 'Posted!'
 
 @app.route('/json')
 def json():
-    return 'Home'
+    return jsonify({'mykey' : 'JSON Value!', 'mylist' : [1, 2, 3, 4, 5]})
+
+@app.route('/dynamic', defaults={'user_input' : 'Default!'})
+@app.route('/dynamic/<user_input>')
+def dyanmic(user_input):
+    return f'The sring the user entered was: { user_input }'
+
+@app.route('/query')
+def query():
+    user_input = request.args.get('user_input')
+    second_input = request.args.get('second_input')
+    return f'The query string contains: { user_input }'
+
+@app.route('/form')
+def form_get():
+    return '<form method="POST><input type="text" name="user_input"><input type="submit"></form>'
+
+@app.route('/form', methods=['POST'])
+def form_post():
+    return 
